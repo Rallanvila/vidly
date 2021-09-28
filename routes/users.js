340ee13768d0b1b53f2d2a40e0b48807732a0ae6
Allan <74523880+Rallanvila@users.noreply.github.com>
2021-09-28 17:32:18 -0400

    finished auths

--- routes/users.js
+++ routes/users.js
@@ -1,59 +1,32 @@
-// const { Rental, validate } = require("../models/rental");
-const { User, validate } = require("../models/user");
-// const { Movie } = require("../models/movie");
-// const { Customer } = require("../models/customer");
-const mongoose = require("mongoose");
-const Fawn = require("fawn");
-const express = require("express");
+const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
 const router = express.Router();
 
-Fawn.init(mongoose);
-
-router.get("/", async (req, res) => {
-	const users = await User.find().sort("name");
-	res.send(users);
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
 });
 
-router.post("/", async (req, res) => {
-	const { error } = validate(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-	const user = await User.findById(req.body.userId);
-	if (!user) return res.status(400).send("Invalid user.");
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send('User already registered.');
 
-	let user = new User({
-		customer: {
-			_id: customer._id,
-			name: customer.name,
-			email: customer.phone,
-			password: customer.password,
-		},
-	});
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
 
-	let result = user.save();
-	console.log(result);
-	//   try {
-	//     new Fawn.Task()
-	//       .save('rentals', rental)
-	//       .update('movies', { _id: movie._id }, {
-	//         $inc: { numberInStock: -1 }
-	//       })
-	//       .run();
-
-	//     res.send(rental);
-	//   }
-	//   catch(ex) {
-	//     res.status(500).send('Something failed.');
-	//   }
+  const token = user.generateAuthToken();
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
-router.get("/:id", async (req, res) => {
-	const user = await User.findById(req.params.id);
-
-	if (!user)
-		return res.status(404).send("The user with the given ID was not found.");
-
-	res.send(user);
-});
-
-module.exports = router;
+module.exports = router; 
